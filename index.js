2e5ec9dc88b89d794927b8e84d8601df9242afbe
bagnalli <stevenbagnall@outlook.com>
2023-05-03 17:22:54 +0100

    server index in root set up, routes folder created

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+// Import route files
+// const pokemonRoutes = require('./routes/pokemon');
+// const userRoutes = require('./routes/user');
+
+// Mount routes on the app
+// app.use('/pokemon', pokemonRoutes);
+// app.use('/user', userRoutes);
+
+module.exports = app;
