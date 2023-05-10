03114bfeb07bcef65017e971403f399bb7ad898e
bagnalli <stevenbagnall@outlook.com>
2023-05-10 18:00:56 +0100

    get requests created

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { sequelize } = require("./db");
 // const { PORT = 4000 } = process.env;
 const port = 3000;
 
-app.listen(port, () => {
-  sequelize.sync({ force: false });
+app.listen(port, async () => {
+  await sequelize.sync({ force: false });
   console.log(`Users are ready at http://localhost:${port}`);
 });
