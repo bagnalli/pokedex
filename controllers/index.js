812f6fd9278e04ffa1663b5f7a93d48d7b5adafd
bagnalli <stevenbagnall@outlook.com>
2023-05-17 13:08:19 +0100

    login token success

--- controllers/index.js
+++ controllers/index.js
@@ -18,4 +18,11 @@ app.get("/", async (req, res, next) => {
   }
 });
 
+// error handling middleware, so failed tests receive them
+app.use((error, req, res, next) => {
+  console.error("SERVER ERROR: ", error);
+  if (res.statusCode < 400) res.status(500);
+  res.send({ error: error.message, name: error.name, message: error.message });
+});
+
 module.exports = app;
