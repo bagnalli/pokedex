5744a8b42d5ec87eacc77880c27004f506b275ac
bagnalli <stevenbagnall@outlook.com>
2023-08-03 11:29:17 +0100

    js fe start

--- controllers/middleware.js
+++
@@ -1,9 +0,0 @@
-// Store middleware for reuse in other files
-const express = require("express");
-
-const middleware = express();
-
-middleware.use(express.json());
-middleware.use(express.urlencoded({ extended: true }));
-
-module.exports = middleware;
